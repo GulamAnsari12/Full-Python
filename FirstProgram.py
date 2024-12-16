9b28d199404d4af5714813572a79c4808e8c9417
Gulam Mohammad <gulamansari0078692@gmail.com>
2024-12-16 22:55:31 +0530

    first commit

---
+++ FirstProgram.py
@@ -0,0 +1,50 @@
+print("hi")
+print("thid is my fisrt programe")
+print ("i hope you all like it "
+       "i love this tutorial")
+
+#for wright multople line(Use triple quotation mark Or \n)
+print("""hello world 
+      this is my first programe 
+      i love this tutorial""")
+
+print("hello world \npython is a interprater language \npython is a high level language.")
+
+#When we use appostrafe
+print ('it\'s going to rain today')
+
+
+#Comments in python(we use # for understandind code)
+#hello
+#"""hi
+# im here 
+# for your help"""
+
+#Create variable
+a= "hello"
+print(a)
+print(type(a))
+
+Name_Gulam = "my name is gulam"
+print(Name_Gulam)
+
+Num123 = (1,33,4,67,8)
+print(Num123)
+print(type(Num123))
+
+num = 1,2,3,4,5
+print(num)
+print(type(num))
+
+num1 = 10120575
+print(type (num1))
+
+#take input from user
+name = input("Enter your name :- ")
+print(name)
+
+age = int(input("Enter age"))
+print(age)
+
+length = float(input("Enter the length of the rectangle"))
+print(length)
